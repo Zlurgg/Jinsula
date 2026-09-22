d4875986aff203158626f65b7119d4234710b360
Zlurgg <joseph.brightman@gmail.com>
2026-09-22 09:17:55 +0100

    Initial Commit

---
+++ Jinsula/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jinsula
+//
+//  Created by Joseph Brightman on 22/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jinsula/JinsulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JinsulaApp.swift
+//  Jinsula
+//
+//  Created by Joseph Brightman on 22/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct JinsulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
